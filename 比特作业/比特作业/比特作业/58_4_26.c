2881c413cedb22500a8b194ae6c5a75a7ff5dc4f
sh2268411762 <2268411762@qq.com>
2020-04-26 21:58:56 +0800

    HomeWork

---
+++ 比特作业/比特作业/比特作业/58_4_26.c
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+////int main()
+////{
+////    int i = 1;
+////    int ret = (++i) + (++i) + (++i);
+////    printf("ret = %d\n", ret);
+////    return 0;
+////}
+//
+//
+//int i;
+//int main()
+//{
+//    i--;
+//    if (i > sizeof(i))
+//    {
+//        printf(">\n");
+//    }
+//    else
+//    {
+//        printf("<\n");
+//    }
+//    return 0;
+//}
