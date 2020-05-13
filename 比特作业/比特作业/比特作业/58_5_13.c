9ae334444dd5dbb72d2e338fe419a0bcf4748b6e
sh2268411762 <2268411762@qq.com>
2020-05-13 22:49:17 +0800

    homework

---
+++ 比特作业/比特作业/比特作业/58_5_13.c
@@ -0,0 +1,112 @@
+//#include < stdio.h >
+//
+////喝汽水，1瓶汽水1元，
+////2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）。
+//
+//int main()
+//{
+//	int money = 20;
+//	int count = 0;//瓶子
+//	int sum = 0;//喝的饮料总数
+//	printf("请输入钱数：-->");
+//	scanf("%d", &money);
+//	for (size_t i = 0; i < 20; i++)
+//	{
+//		count++;
+//		sum++;
+//		money--;
+//	}
+//	while (count / 2)
+//	{
+//		count -= 2;
+//		count++;
+//		sum++;
+//	}
+//	printf("喝的饮料总数 sum = %d\n", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//
+//
+//	scanf("%d", &money);
+//
+//	//方法1
+//	total = money;
+//	empty = money;
+//	while (empty > 1)
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty % 2;
+//	}
+//	printf("%d\n", total);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//
+//
+//	scanf("%d", &money);
+//
+//	//方法2
+//	if (money <= 0)
+//	{
+//		total = 0;
+//	}
+//	else
+//	{
+//		total = money * 2 - 1;
+//	}
+//	printf("total = %d\n", total);
+//
+//
+//	return 0;
+//}
+
+//struct S
+//{
+//	int a;
+//	int b;
+//};
+//
+//struct stu
+//{
+//	int num;
+//	char name[10];
+//	int age;
+//};
+//
+//
+//void fun(struct stu* p)
+//{
+//	printf("%s\n", (*p).name);
+//	return;
+//}
+//
+//
+//int main()
+//{
+//	struct stu students[3] = { {9801,"zhang",20},
+//							  {9802,"wang",19},
+//							  {9803,"zhao",18}
+//	};
+//	fun(students + 1);
+//	return 0;
+//}
+//int main()
+//{
+//	struct S a, * p = &a;
+//	a.a = 99;
+//	printf("%d\n", a.a);
+//	//printf("%d\n", *p.a);
+//	printf("%d\n", p->a);
+//	printf("%d\n", (*p).a);
+//	return 0;
+//}
