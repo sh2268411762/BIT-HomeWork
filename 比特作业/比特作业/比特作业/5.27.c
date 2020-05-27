74785bebd2374a0af95601ceb74525693da3e357
sh2268411762 <2268411762@qq.com>
2020-05-27 23:08:12 +0800

    27号作业

---
+++ 比特作业/比特作业/比特作业/5.27.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//int main()
+//{
+//    char str1[] = "hello bit.";
+//    char str2[] = "hello bit.";
+//    char* str3 = "hello bit.";
+//    char* str4 = "hello bit.";
+//    if (str1 == str2)
+//        printf("str1 and str2 are same\n");
+//    else
+//        printf("str1 and str2 are not same\n");
+//
+//    if (str3 == str4)
+//        printf("str3 and str4 are same\n");
+//    else
+//        printf("str3 and str4 are not same\n");
+//
+//    return 0;
+//}
