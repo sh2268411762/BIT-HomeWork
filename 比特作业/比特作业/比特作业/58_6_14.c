26695a0d09ce9b05002feea0b3a5285f46c193a1
sh2268411762 <2268411762@qq.com>
2020-06-15 21:17:29 +0800

    6.14作业

---
+++ 比特作业/比特作业/比特作业/58_6_14.c
@@ -0,0 +1,49 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+
+//
+//struct A
+//{
+//	int a;
+//	char b;
+//	short c;
+//	short d;
+//}a;
+//int main()
+//{
+//	printf("%d %d\n", sizeof(a), sizeof(long));
+//	return 0;
+//}
+
+
+//int main(int argc, char* argv[])
+//{
+//    struct tagTest1
+//    {
+//        short a;
+//        char d;
+//        long b;
+//        long c;
+//    };
+//    struct tagTest2
+//    {
+//        long b;
+//        short c;
+//        char d;
+//        long a;
+//    };
+//    struct tagTest3
+//    {
+//        short c;
+//        long b;
+//        char d;
+//        long a;
+//    };
+//    struct tagTest1 stT1;
+//    struct tagTest2 stT2;
+//    struct tagTest3 stT3;
+//
+//    printf("%d %d %d", sizeof(stT1), sizeof(stT2), sizeof(stT3));
+//    return 0;
+//}
+//#pragma pack()
